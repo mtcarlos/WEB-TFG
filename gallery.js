deafeb85c8ce16c2e4a933d8d65b77d8190cdaa2
mtcarlos <mtcarlos2003@gmail.com>
2026-09-14 12:29:31 +0200

    Nuevo tutorial

--- gallery.js
+++ gallery.js
@@ -46,7 +46,7 @@
       videoGallery.style.display = 'grid';
       imageGallery.style.display = 'none';
       if (galleryHeading) galleryHeading.textContent = 'La Ciudad en Vídeos';
-      if (galleryCount) galleryCount.textContent = '01 vídeo';
+      if (galleryCount) galleryCount.textContent = '02 vídeos';
     });
   }
 
